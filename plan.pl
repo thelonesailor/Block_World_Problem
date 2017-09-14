ea5656ea776407b3ec6593a3caedc920b6eac48b
abhudev <abhipathak97@gmail.com>
2017-09-15 04:40:35 +0530

    Added list functions

--- plan.pl
+++ plan.pl
@@ -1,6 +1,25 @@
-:- use_module(library(lists)).
+%% :- use_module(library(lists)).
 :- set_prolog_flag(answer_write_options,[quoted(true),portray(true),spacing(next_argument)]).
 
+%% append two lists
+append([], L, L).
+append([A|L1], L2, [A|L]) :- append(L1, L2, L).
+
+%% find element in a list
+in_list(A, [B|L]) :- A \= B, in_list(A, L).
+in_list(A, [A|L]).
+
+%% FInd union of two lists
+union([], L, L).
+%% X is in L2
+union([X|L1], L2, L) :- in_list(X, L2), union(L1, L2, L).
+%% X is not in L2
+union([X|L1], L2, [X|L]) :- \+ in_list(X, L2), union(L1, L2, L).
+
+%% Subtract one list form another
+subtract([], Ls, []).
+subtract([X|L1], L2, L) :- in_list(X, L2), subtract(L1, L2, L).
+subtract([X|L1], L2, [X|L]) :- \+ in_list(X, L2), subtract(L1, L2, L).
 
 %% States are sets of predicates(don't want to repeat predicates)
 %% Start, Goal, Plan
